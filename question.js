eed61f9e7b2f2effcb25c8559f742440956ef513
Easwari Ramani <eshuprad@gmail.com>
2018-01-11 08:31:33 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,17 @@
 'use strict';
+class Question{
+  static All()
+  {
+    return this._All;
+  }
+  constructor(){
+    
+  }
+  save(){
+    
+  }
+  static FIND(ID){
+    
+  }
+}
+Question._All=[];
