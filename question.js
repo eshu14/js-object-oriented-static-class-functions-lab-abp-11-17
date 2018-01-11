8ed1158038c701280d38073ade45c523a3cdc5e2
Easwari Ramani <eshuprad@gmail.com>
2018-01-11 10:05:48 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -14,7 +14,7 @@ class Question{
 
   static Find(ID){
         return this.All().filter(function(ID){
-          return this._All[ID-1];
+          return this.question===question;
       })
   }
 }
