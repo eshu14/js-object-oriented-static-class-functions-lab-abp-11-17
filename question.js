345bf80dc409e127e14db3372a16545ed9271a78
Easwari Ramani <eshuprad@gmail.com>
2018-01-11 10:26:03 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,24 @@
 'use strict';
+class Question
+{
+  constructor(content){
+    this.content=content;
+    this.save();
+  }
+  save(){
+    this.constructor._All.push(this);
+  }
+  static All()
+  {
+    return this._All;
+  }
+
+  static Find(ID){
+      return this._All[ID-1];
+    }
+}
+Question._All=[];
+/*const question1 = new Question("What's your favorite food?");
+const question2 = new Question("What city were you born in?");
+const question3 = new Question("What's your favorite TV Show");*/
+//question.save();
